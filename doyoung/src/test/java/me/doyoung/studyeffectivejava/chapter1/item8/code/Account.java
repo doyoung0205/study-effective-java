fbca8753e2ec2252b1b02261bd9c604572adc01e
김도영 <doyoung0205@naver.com>
2022-03-15 22:09:32 +0900

    feat : add finallizerAttack

---
+++ doyoung/src/test/java/me/doyoung/studyeffectivejava/chapter1/item8/code/Account.java
@@ -0,0 +1,17 @@
+package me.doyoung.studyeffectivejava.chapter1.item8.code;
+
+public class Account {
+    private String name;
+
+    public Account(String name) {
+        this.name = name;
+        if (this.name.equals("푸틴")) {
+            throw new IllegalArgumentException("푸틴은 안 돼~");
+        }
+    }
+
+    public void transfer(int amount, String to) {
+        System.out.printf("transfer %d from %s to %s", amount, this.name, to);
+    }
+
+}
