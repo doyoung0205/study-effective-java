bf0d347c068ea2713f5fb44bd6e6b1ba0830da64
김도영 <doyoung0205@naver.com>
2022-03-19 04:55:50 +0900

    docs : item22 ~ item25 정리

---
+++ doyoung/src/main/java/me/doyoung/studyeffectivejava/chapter3/item25/Main.java
@@ -0,0 +1,8 @@
+package me.doyoung.studyeffectivejava.chapter3.item25;
+
+// (150쪽)
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Utensil.NAME + Dessert.NAME);
+    }
+}
---
+++ doyoung/src/test/java/me/doyoung/studyeffectivejava/chapter3/item24/code/ClazzTest.java
@@ -0,0 +1,7 @@
+package me.doyoung.studyeffectivejava.chapter3.item24.code;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClazzTest {
+
+}
