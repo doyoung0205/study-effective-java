0513c0518d6ff95abeb31338ba5ebced8e8c6284
haedoang <haedoang@gmail.com>
2022-03-20 16:10:59 +0900

    feat: item 27 내용 정리

--- haedoang/src/main/java/chapter5/item27/ArrayUtil.java
+++ haedoang/src/main/java/chapter5/item27/ArrayUtil.java
@@ -11,8 +11,16 @@ public class ArrayUtil {
     private ArrayUtil() {
     }
 
+    /**
+     * 배열을 복사하는 메서드입니다. <br/>
+     * 같은 타입의 배열을 복사하기 때문에 타입의 안정성을 보장합니다 <br/>
+     *
+     * @param t
+     * @param <T>
+     * @return
+     */
     public static <T> T[] arrayCopy(T[] t) {
-//        @SuppressWarnings("unchecked")
+        @SuppressWarnings("unchecked")
         final T[] copiedArray = (T[]) Arrays.copyOf(t, t.length, t.getClass());
         return copiedArray;
     }
