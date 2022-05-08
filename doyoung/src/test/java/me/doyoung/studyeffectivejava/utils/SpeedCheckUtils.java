023f50d0ee50c9b1f1574f03d9a327281f951956
김도영 <doyoung0205@naver.com>
2022-05-09 00:23:39 +0900

    docs : 48, 49 items

--- doyoung/src/test/java/me/doyoung/studyeffectivejava/utils/SpeedCheckUtils.java
+++ doyoung/src/test/java/me/doyoung/studyeffectivejava/utils/SpeedCheckUtils.java
@@ -1,5 +1,7 @@
 package me.doyoung.studyeffectivejava.utils;
 
+import org.junit.platform.commons.util.ReflectionUtils;
+
 public class SpeedCheckUtils {
 
     private static final String DEFAULT_TASK_NAME = "result";
@@ -8,15 +10,14 @@ public class SpeedCheckUtils {
         throw new AssertionError();
     }
 
-    public static void check(Runnable runnable) {
-        check(DEFAULT_TASK_NAME, runnable);
+    public static void speedCheck(Runnable runnable) {
+        speedCheck(DEFAULT_TASK_NAME, runnable);
     }
 
-    public static void check(String taskName, Runnable runnable) {
+    public static void speedCheck(String taskName, Runnable runnable) {
         long st = System.currentTimeMillis();
         runnable.run();
         long et = System.currentTimeMillis();
-
-        System.out.printf("%s :: \'%d\' ms\n", taskName, (et - st));
+        System.out.printf("%s :: '%d' ms\n", taskName, (et - st));
     }
 }
