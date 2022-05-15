e7422c0a65ea483ee75653fc0a8cd779756ee2fa
김도영 <doyoung0205@naver.com>
2022-05-15 22:16:01 +0900

    docs : item 51

--- doyoung/src/test/java/me/doyoung/studyeffectivejava/chapter3/item24/code/ClazzTest.java
+++ doyoung/src/test/java/me/doyoung/studyeffectivejava/chapter3/item24/code/ClazzTest.java
@@ -1,7 +1,17 @@
 package me.doyoung.studyeffectivejava.chapter3.item24.code;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClazzTest {
 
+    @Test
+    void test() {
+        final Clazz clazz = new Clazz();
+        Clazz.PublicInner publicInner = clazz.new PublicInner();
+        publicInner = null;
+        assertThat(clazz).isNotNull();
+    }
 }
